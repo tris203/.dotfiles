ee2d184f25564ba594fe41d26619d798c3ec7f1e
tris203 <admin@snappeh.com>
2023-12-24 23:09:06 +0000

    gutter colour

--- nvim/lua/custom/plugins/cs_tokyonight.lua
+++ nvim/lua/custom/plugins/cs_tokyonight.lua
@@ -1,9 +1,17 @@
 return {
-  { 'folke/tokyonight.nvim', priority = 1000, lazy = false, opts = {
-transparent = true,
-styles = {
-sidebars = "transparent",
-floats = "transparent",
+  {
+    'folke/tokyonight.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      transparent = true,
+      on_colors = function(colors)
+        colors.fg_gutter = '#b2b8cf'
+      end,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
     },
-  }},
+  },
 }
