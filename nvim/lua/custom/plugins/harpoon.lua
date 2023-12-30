0bc56d2f889a2fc89b33ac979be0e3e57f11594a
tris203 <admin@snappeh.com>
2023-12-30 00:08:41 +0000

    harpoon lazy load

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -1,43 +1,53 @@
+local harpoon
 return {
   {
     'theprimeagen/harpoon',
     branch = 'harpoon2',
-    -- dir = "/home/tris/code/harpoon",
-    event = 'VeryLazy',
+    dev = false,
     config = function()
-      local harpoon = require 'harpoon'
-
-      -- harpoon2 keybinds
-      vim.keymap.set('n', '<leader>a', function()
-        harpoon:list():append()
-      end)
-
-      vim.keymap.set('n', '<leader>h', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list 'tmux')
-      end)
-      vim.keymap.set('n', '<C-e>', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
-      end)
-      vim.keymap.set('n', '<C-t>', function()
-        harpoon:list():select(1)
-      end)
-      vim.keymap.set('n', '<C-h>', function()
-        harpoon:list():select(2)
-      end)
-      vim.keymap.set('n', '<C-n>', function()
-        harpoon:list():select(3)
-      end)
-      vim.keymap.set('n', '<C-b>', function()
-        harpoon:list():select(4)
-      end)
-
-      harpoon:setup {
-        default = {},
-      }
+      harpoon = require 'harpoon'
+      harpoon:setup {}
     end,
-
     dependencies = {
       'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      {
+        '<leader>a',
+        function()
+          harpoon:list():append()
+        end,
+      },
+      {
+        '<C-e>',
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+      },
+      {
+        '<C-t>',
+        function()
+          harpoon:list():select(1)
+        end,
+      },
+      {
+        '<C-h>',
+        function()
+          harpoon:list():select(2)
+        end,
+      },
+      {
+        '<C-n>',
+        function()
+          harpoon:list():select(3)
+        end,
+      },
+      {
+        '<C-b>',
+        function()
+          harpoon:list():select(4)
+        end,
+      },
     },
   },
 }
