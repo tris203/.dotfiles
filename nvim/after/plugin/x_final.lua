4040e6ffcd2d5cec294a30ae974e280c1d5073a8
tris203 <admin@snappeh.com>
2023-12-30 13:21:09 +0000

    pcall intro for direct files

--- nvim/after/plugin/x_final.lua
+++ nvim/after/plugin/x_final.lua
@@ -1,9 +1,12 @@
 vim.api.nvim_create_autocmd('User', {
   pattern = 'LazyVimStarted',
   callback = function()
+  local ok =  pcall(function()
     local intro = require('custom.intro')
     intro.show()
+    end)
+    if not ok then
+      vim.print('Intro Skipped')
+    end
     end
     })
-
-print 'Ready to go!'
