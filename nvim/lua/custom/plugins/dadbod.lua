6178d932c8a435add59fad1849abc4869b4bd3e5
tris203 <admin@snappeh.com>
2024-08-26 22:40:47 +0100

    fix(dadbod): change config to init function

--- nvim/lua/custom/plugins/dadbod.lua
+++ nvim/lua/custom/plugins/dadbod.lua
@@ -13,7 +13,7 @@ return {
   keys = {
     { '<leader>db', '<cmd>DBUIToggle<CR>', desc = "Open DBUI" },
   },
-  config = function()
+  init = function()
     -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
   end,
