855ddfb53ed2ada29ac2f7d4ecf22bc58142db6f
tris203 <admin@snappeh.com>
2025-10-24 16:37:29 +0100

    feat(boundary): add boundary.nvim for React file boundaries
    Added the 'Kenzo-Wada/boundary.nvim' plugin to highlight logical
    boundaries in TypeScript React and JavaScript React files. Enabled
    automatic refresh by default. Updated lazy-lock.json to reflect the new
    plugin and other dependency updates.

---
+++ nvim/lua/custom/plugins/boundary.lua
@@ -0,0 +1,9 @@
+return {
+  'Kenzo-Wada/boundary.nvim',
+  branch = 'release',
+  opts = {
+    auto = true, -- automatic refresh enabled by default
+    -- marker_text = "'use client'",
+  },
+  ft = { 'typescriptreact', 'javascriptreact' },
+}
