00d5619b4064129d1406bac1f082c5d31a2bf490
tris203 <admin@snappeh.com>
2026-04-25 21:48:50 +0100

    fix(neojj): set correct filetype for jjcommit buffers
    Add an autocmd to change the filetype of 'jjcommit' buffers to
    'jjdescription' for better syntax handling. This ensures proper
    highlighting and behavior when editing commit messages with neojj.

--- nvim/lua/custom/plugins/jj.lua
+++ nvim/lua/custom/plugins/jj.lua
@@ -4,4 +4,12 @@ return {
   'NicholasZolton/neojj',
   event = 'VeryLazy',
   opts = {},
+  init = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'jjcommit',
+      callback = function(ev)
+        vim.bo[ev.buf].filetype = 'jjdescription'
+      end,
+    })
+  end,
 }
