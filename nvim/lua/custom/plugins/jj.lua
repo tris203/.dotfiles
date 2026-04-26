02c745cf57d5c613c61ca7760f0edf91c0296821
tris203 <admin@snappeh.com>
2026-04-26 20:47:17 +0100

    fix(neojj): remove redundant FileType autocmd
    upstreamed

--- nvim/lua/custom/plugins/jj.lua
+++ nvim/lua/custom/plugins/jj.lua
@@ -4,12 +4,4 @@ return {
   'NicholasZolton/neojj',
   event = 'VeryLazy',
   opts = {},
-  init = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = 'jjcommit',
-      callback = function(ev)
-        vim.bo[ev.buf].filetype = 'jjdescription'
-      end,
-    })
-  end,
 }
